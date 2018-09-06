81156f8a7685698c5721e6aca311d9966f3f9ca2
PavelSindler <sindlerpa@gmail.com>
2018-09-06 14:29:42 +0200

    lang changes

--- lang/config.sh
+++ lang/config.sh
@@ -5,7 +5,7 @@
 #  This file is 'included' in all scripts.
 #
 # Arduino main folder:
-export ARDUINO=C:/arduino-1.6.8
+export ARDUINO=C:/arduino-1.6.8/arduino-1.6.8
 #
 # Arduino builder:
 export BUILDER=$ARDUINO/arduino_debug.exe
