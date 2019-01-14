78d90e00c951cac467519f80b17416e2707941a3
PavelSindler <sindlerpa@gmail.com>
2019-01-14 18:35:43 +0100

    7x7 points

--- Firmware/variants/1_75mm_MK3-EINSy10a-E3Dv6full.h
+++ Firmware/variants/1_75mm_MK3-EINSy10a-E3Dv6full.h
@@ -400,8 +400,8 @@
 #define MESH_NUM_X_POINTS 7
 #define MESH_NUM_Y_POINTS 7
 // Mesh measure definition
-#define MESH_MEAS_NUM_X_POINTS 3
-#define MESH_MEAS_NUM_Y_POINTS 3
+#define MESH_MEAS_NUM_X_POINTS 7
+#define MESH_MEAS_NUM_Y_POINTS 7
 
 #define MESH_HOME_Z_CALIB 0.2
 #define MESH_HOME_Z_SEARCH 5 //Z lift for homing, mesh bed leveling etc.
